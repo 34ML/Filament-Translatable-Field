06efedd2b855c66614bccb802a86016ee5495b68
Mostafa Hassan <mostafa@34ml.com>
2023-12-06 10:56:41 +0200

    Fix hidden fields not being saved creation

--- src/Forms/Components/LanguageSelector.php
+++ src/Forms/Components/LanguageSelector.php
@@ -3,6 +3,7 @@
 namespace _34ML\FilamentTranslatableField\Forms\Components;
 
 use Closure;
+use Filament\Forms\Components\Component;
 use Filament\Forms\Components\Field;
 use Illuminate\Contracts\Support\Htmlable;
 
@@ -19,6 +20,13 @@ class LanguageSelector extends Field
             ->live()
             ->default(array_keys(config('filament-translatable-field.locales'))[0] ?? 'en')
             ->dehydrated(false)
+            ->beforeStateDehydrated(function($state,Component $component) {
+                $components = $component->getContainer()->getComponents(true);
+                foreach ($components as $component) {
+                    if($component->isHidden())
+                        $component->hidden(false);
+                }
+            })
             ->columnSpan('full');
     }
 
